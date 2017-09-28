b488bb8ca7dbd47acf86dfc68fec78fc9c5ff8b6
Coty Condry <ccondry@cisco.com>
2017-09-28 06:59:47 -0500

    always pop up the session modal on page load

--- src/store/actions.js
+++ src/store/actions.js
@@ -277,6 +277,9 @@ export const checkSession = ({state, commit}, qs) => {
   } else {
     // we don't need to ask on this one
   }
+
+  // always pop up modal to ask for session info
+  this.commit(types.SET_NEEDS_SESSION, true)
 }
 
 export const setNeedsSession = ({commit, state, rootState}, data) => {
