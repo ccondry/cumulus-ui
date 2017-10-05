cde7dc7815b38b65292326cc9c9e097140080d8d
Coty Condry <ccondry@cisco.com>
2017-10-04 23:14:44 -0600

    add placeholder prop

--- src/components/vertical-formgroup.vue
+++ src/components/vertical-formgroup.vue
@@ -2,7 +2,7 @@
   <div class="form-group row">
     <label v-if="$slots['default']" class="col-sm-3 col-form-label" :style="private ? 'color: red;' : ''"><slot></slot></label>
     <div class="col-sm-6">
-      <input class="form-control" :type="private ? 'password' : ''">
+      <input class="form-control" :type="private ? 'password' : ''" :placeholder="placeholder">
     </div>
   </div>
 </template>
@@ -13,7 +13,8 @@ export default {
   props: {
     private: {
       default: false
-    }
+    },
+    placeholder: {}
   }
 }
 </script>
