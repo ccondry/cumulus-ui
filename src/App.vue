ef9e7db9ceac865cc9c332e637dc19f5bac8cb92
Coty Condry <ccondry@cisco.com>
2018-12-07 09:48:54 -0600

    whitespace

--- src/App.vue
+++ src/App.vue
@@ -109,7 +109,7 @@ export default {
     // this.getSessionInfo()
     // get REST endpoints info
     await this.getEndpoints()
-    
+
     this.getVerticalConfig()
 
     // update the HTML page title
